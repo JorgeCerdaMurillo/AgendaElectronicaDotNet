e7f90121bd0946659087aea675f46530e771bd67
JorgeCerdaMurillo <cerdamurillojorge00@gmail.com>
2023-03-03 23:08:12 -0600

    a

--- AgendaElectronica/Form1.Designer.cs
+++ AgendaElectronica/Form1.Designer.cs
@@ -28,13 +28,180 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbNombres = new System.Windows.Forms.TextBox();
+            this.tbApellidos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbTelefono = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnBorrar = new System.Windows.Forms.Button();
+            this.btnGuardarArchivo = new System.Windows.Forms.Button();
+            this.dgvLista = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombres:";
+            // 
+            // tbNombres
+            // 
+            this.tbNombres.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbNombres.Location = new System.Drawing.Point(26, 43);
+            this.tbNombres.Name = "tbNombres";
+            this.tbNombres.Size = new System.Drawing.Size(353, 29);
+            this.tbNombres.TabIndex = 1;
+            // 
+            // tbApellidos
+            // 
+            this.tbApellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbApellidos.Location = new System.Drawing.Point(26, 111);
+            this.tbApellidos.Name = "tbApellidos";
+            this.tbApellidos.Size = new System.Drawing.Size(353, 29);
+            this.tbApellidos.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Apellidos:";
+            // 
+            // tbTelefono
+            // 
+            this.tbTelefono.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTelefono.Location = new System.Drawing.Point(26, 181);
+            this.tbTelefono.Name = "tbTelefono";
+            this.tbTelefono.Size = new System.Drawing.Size(353, 29);
+            this.tbTelefono.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(22, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Teléfono:";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbEmail.Location = new System.Drawing.Point(26, 252);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(353, 29);
+            this.tbEmail.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(22, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "E-mail:";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgregar.Location = new System.Drawing.Point(415, 50);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(184, 58);
+            this.btnAgregar.TabIndex = 8;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
+            // 
+            // btnBorrar
+            // 
+            this.btnBorrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBorrar.Location = new System.Drawing.Point(415, 133);
+            this.btnBorrar.Name = "btnBorrar";
+            this.btnBorrar.Size = new System.Drawing.Size(184, 58);
+            this.btnBorrar.TabIndex = 9;
+            this.btnBorrar.Text = "Borrar";
+            this.btnBorrar.UseVisualStyleBackColor = true;
+            this.btnBorrar.Click += new System.EventHandler(this.btnBorrar_Click);
+            // 
+            // btnGuardarArchivo
+            // 
+            this.btnGuardarArchivo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardarArchivo.Location = new System.Drawing.Point(415, 215);
+            this.btnGuardarArchivo.Name = "btnGuardarArchivo";
+            this.btnGuardarArchivo.Size = new System.Drawing.Size(184, 58);
+            this.btnGuardarArchivo.TabIndex = 10;
+            this.btnGuardarArchivo.Text = "Grabar Archivo";
+            this.btnGuardarArchivo.UseVisualStyleBackColor = true;
+            this.btnGuardarArchivo.Click += new System.EventHandler(this.btnGuardarArchivo_Click);
+            // 
+            // dgvLista
+            // 
+            this.dgvLista.AllowUserToAddRows = false;
+            this.dgvLista.AllowUserToDeleteRows = false;
+            this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLista.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvLista.Location = new System.Drawing.Point(26, 309);
+            this.dgvLista.Name = "dgvLista";
+            this.dgvLista.ReadOnly = true;
+            this.dgvLista.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvLista.Size = new System.Drawing.Size(573, 299);
+            this.dgvLista.TabIndex = 11;
+            this.dgvLista.DoubleClick += new System.EventHandler(this.dgvLista_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(632, 632);
+            this.Controls.Add(this.dgvLista);
+            this.Controls.Add(this.btnGuardarArchivo);
+            this.Controls.Add(this.btnBorrar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbTelefono);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbApellidos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbNombres);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNombres;
+        private System.Windows.Forms.TextBox tbApellidos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbTelefono;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnBorrar;
+        private System.Windows.Forms.Button btnGuardarArchivo;
+        private System.Windows.Forms.DataGridView dgvLista;
     }
 }
 
--- AgendaElectronica/Form1.cs
+++ AgendaElectronica/Form1.cs
@@ -7,14 +7,126 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace AgendaElectronica
 {
     public partial class Form1 : Form
     {
+        private List<Contacto> contactoList=new List<Contacto>();
+        private int indice = -1;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamReader lector = new StreamReader("AgendaElectronica.txt");
+                string linea;
+                while ((linea = lector.ReadLine()) != null)
+                {
+                    int posicion;
+                    Contacto persona = new Contacto();
+                    posicion = linea.IndexOf("|");
+                    persona.Nombres = linea.Substring(0, posicion);
+                    linea = linea.Substring(++posicion);
+                    posicion = linea.IndexOf("|");
+                    persona.Apellidos = linea.Substring(0, posicion);
+                    linea = linea.Substring(++posicion);
+                    posicion = linea.IndexOf("|");
+                    persona.Telefono = linea.Substring(0, posicion);
+                    linea = linea.Substring(++posicion);
+                    posicion = linea.IndexOf("|");
+                    persona.Email = linea.Substring(0, posicion);
+                    contactoList.Add(persona);
+                }
+                lector.Close();
+                actualizaVista();
+            }
+             catch (Exception ex)
+            {
+                Console.WriteLine("Exception: "+ ex.Message);
+               
+            }
+            finally
+            {
+                Console.WriteLine("Lista Cargada");
+            }
+
+        }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            Contacto persona = new Contacto();
+            persona.Nombres=tbNombres.Text;
+            persona.Apellidos=tbApellidos.Text;
+            persona.Telefono=tbTelefono.Text;
+            persona.Email=tbEmail.Text;
+            if (indice>-1)
+            {
+                contactoList[indice] = persona;
+                indice = -1;
+
+            }
+            else
+            {
+                contactoList.Add(persona);
+            }
+            actualizaVista();
+            limpiarcampos();
+
+        }
+        private void actualizaVista()
+        {
+            dgvLista.DataSource = null;
+            dgvLista.DataSource = contactoList;
+            dgvLista.ClearSelection();
+
+        }
+
+        private void dgvLista_DoubleClick(object sender, EventArgs e)
+        {
+            DataGridViewRow renglon = dgvLista.SelectedRows[0];
+            indice = dgvLista.Rows.IndexOf(renglon);
+            Contacto persona = contactoList[indice];
+            tbNombres.Text = persona.Nombres;
+            tbApellidos.Text = persona.Apellidos;
+            tbTelefono.Text = persona.Telefono;
+            tbEmail.Text = persona.Email;
+        }
+        private void limpiarcampos()
+        {
+            tbNombres.Text = "";
+            tbApellidos.Text = "";
+            tbTelefono.Text = "";
+            tbEmail.Text = "";
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if (indice>-1)
+            {
+                contactoList.RemoveAt(indice);
+                actualizaVista();
+                limpiarcampos();
+                indice = -1;
+            }
+        }
+
+        private void btnGuardarArchivo_Click(object sender, EventArgs e)
+        {
+            TextWriter Escribir = new StreamWriter("AgendaElectronica.txt");
+            foreach (Contacto persona in contactoList)
+            {
+                Escribir.WriteLine(persona.Nombres+"|"+ persona.Apellidos + "|" + persona.Telefono + "|" + persona.Email + "|");
+            }
+            Escribir.Close();
+            MessageBox.Show("Agenda Creada");
         }
     }
 }
